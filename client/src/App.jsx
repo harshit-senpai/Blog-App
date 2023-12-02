73492acc62a7414478e3cf9fc74630155f4e7313
harshit Mishra <miharshit2@gmail.com>
2023-12-03 01:28:18 +0530

    auth route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navebar.component";
 
 function App() {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Sign In Page</h1>} />
+        <Route path="signup" element={<h1>Sign Up Page</h1>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
